2a12d4e57a6dc905329dab83dcfd2b0f4faf36e2
fuadhasen <fuadhas6634@gmail.com>
2024-02-01 07:12:16 -0800

    pint

--- push_pall.c
+++ push_pall.c
@@ -1,4 +1,5 @@
 #include "monty.h"
+
 /**
  * push - Pushes an element onto the stack
  * @head: Pointer to the head of the stack
@@ -8,23 +9,23 @@
  */
 void push(stack_t **head, unsigned int line_num)
 {
-        stack_t *new;
+	stack_t *new;
 
-        (void)(line_num);
-        new = malloc(sizeof(stack_t));
-        if (new == NULL)
-        {
-                fprintf(stderr, "Error: malloc failed");
-                exit(EXIT_FAILURE);
-        }
-        new->n = value;
-        new->next = *head;
-        new->prev = NULL;
-    if (*head != NULL)
-        {
-                (*head)->prev = new;
-        }
-        *head = new;
+	(void)(line_num);
+	new = malloc(sizeof(stack_t));
+	if (new == NULL)
+	{
+		fprintf(stderr, "Error: malloc failed\n");
+		exit(EXIT_FAILURE);
+	}
+	new->n = value;
+	new->next = *head;
+	new->prev = NULL;
+	if (*head != NULL)
+	{
+		(*head)->prev = new;
+	}
+	*head = new;
 }
 
 /**
@@ -36,25 +37,37 @@ void push(stack_t **head, unsigned int line_num)
  */
 void pall(stack_t **head, unsigned int line_num)
 {
-        stack_t *temp = *head;
+	stack_t *temp = *head;
 
-        (void)(line_num);
-        if (*head == NULL)
-        {
-                return;
-        }
-        while (temp != NULL)
-        {
-                printf("%d\n", temp->n);
-                temp = temp->next;
-        }
+	(void)(line_num);
+	if (*head == NULL)
+	{
+		return;
+	}
+	while (temp != NULL)
+	{
+		printf("%d\n", temp->n);
+		temp = temp->next;
+	}
 }
+
+/**
+ * pint - Prints the value at the top of the stack
+ * @head: Pointer to the head of the stack
+ * @line_num: Line number where the operation occurs
+ *
+ * This function prints the integer value stored at the top of the stack.
+ * If the stack is empty, it prints an error message and exits with failure.
+ *
+ * Return: void
+ */
 void pint(stack_t **head, unsigned int line_num)
 {
-        if (*head == NULL)
-        {
-                fprintf(stderr, "L%u: can't pint, stack empty\n", line_num);
-                exit(EXIT_FAILURE);
-        }
-        printf("%d\n", (*head)->n);
+	if (*head == NULL)
+	{
+		fprintf(stderr, "L%u: can't pint, stack empty\n", line_num);
+		exit(EXIT_FAILURE);
+	}
+	printf("%d\n", (*head)->n);
 }
+
