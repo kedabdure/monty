f1f36bc82bdc6fbfee09c1dff0fcfba5f14c03fb
Abdurehim <128035951+kedabdure@users.noreply.github.com>
2024-02-02 02:50:35 +0300

    Update monty.h
    function header edited

--- monty.h
+++ monty.h
@@ -1,49 +1,34 @@
-#ifndef MAIN
-#define MAIN
+#ifndef MONTY_H
+#define MONTY_H
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
 
-/**
- * struct stack_s - doubly linked list representation of a stack (or queue)
- * @n: integer
- * @prev: points to the previous element of the stack (or queue)
- * @next: points to the next element of the stack (or queue)
- *
- * Description: doubly linked list node structure
- * for stack, queues, LIFO, FIFO
- */
+/* Define stack structure */
 typedef struct stack_s
 {
-        int n;
-        struct stack_s *prev;
-        struct stack_s *next;
+    int n;
+    struct stack_s *prev;
+    struct stack_s *next;
 } stack_t;
 
-extern int value;
-/**
- * struct instruction_s - opcode and its function
- * @opcode: the opcode
- * @f: function to handle the opcode
- *
- * Description: opcode and its function
- * for stack, queues, LIFO, FIFO
- */
+/* Define instruction structure */
 typedef struct instruction_s
 {
-        char *opcode;
-        void (*f)(stack_t **stack, unsigned int line_number);
+    char *opcode;
+    void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 
-void readMontyFile(char *filename);
-void push(stack_t **head, unsigned int line_num);
-void pall(stack_t **head, unsigned int line_num);
-void pint(stack_t **head, unsigned int line_num);
-void processIns(char *instruction, stack_t **head, unsigned int line_num);
-void nop(stack_t **head, unsigned int line_num);
+/* External declaration for global variables */
+extern int value;
 
-#endif /* MAIN */
+/* Function prototypes */
+void free_stack(stack_t *stack);
+void execute(char *opcode, stack_t **stack, unsigned int line_number);
+void parse_monty(FILE *file, stack_t **stack);
+void push(stack_t **stack, unsigned int line_number);
+void pop(stack_t **stack, unsigned int line_number);
+void pint(stack_t **stack, unsigned int line_number);
+void pall(stack_t **stack, unsigned int line_number);
+
+#endif /* MONTY_H */
