c58db4d76ab9d9b265ba05daf925cd473a009da8
Abdurehim <128035951+kedabdure@users.noreply.github.com>
2024-02-02 02:59:56 +0300

    Update and rename bytecodes.m to stack_operations1.c
    the content of this file is totally changed.

---
+++ stack_operations1.c
@@ -0,0 +1,82 @@
+#include "monty.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * push - Push an element onto the stack
+ * @stack: Pointer to the stack
+ * @line_number: Line number of the instruction
+ */
+void push(stack_t **stack, unsigned int line_number)
+{
+    (void)line_number;
+
+    stack_t *new_node = malloc(sizeof(stack_t));
+    if (new_node == NULL)
+    {
+        fprintf(stderr, "Error: malloc failed\n");
+        exit(EXIT_FAILURE);
+    }
+    new_node->n = value;
+    new_node->prev = NULL;
+    if (*stack != NULL)
+        (*stack)->prev = new_node;
+    new_node->next = *stack;
+    *stack = new_node;
+}
+
+/**
+ * pop - Pop an element from the stack
+ * @stack: Pointer to the stack
+ * @line_number: Line number of the instruction
+ */
+void pop(stack_t **stack, unsigned int line_number)
+{
+    (void)line_number;
+
+    stack_t *temp;
+    if (*stack == NULL)
+    {
+        fprintf(stderr, "Error: cannot pop from an empty stack\n");
+        exit(EXIT_FAILURE);
+    }
+    temp = *stack;
+    *stack = (*stack)->next;
+    if (*stack != NULL)
+        (*stack)->prev = NULL;
+    free(temp);
+}
+
+/**
+ * pint - Print the value at the top of the stack
+ * @stack: Pointer to the stack
+ * @line_number: Line number of the instruction
+ */
+void pint(stack_t **stack, unsigned int line_number)
+{
+    (void)line_number;
+
+    if (*stack == NULL)
+    {
+        fprintf(stderr, "Error: cannot pint an empty stack\n");
+        exit(EXIT_FAILURE);
+    }
+    printf("%d\n", (*stack)->n);
+}
+
+/**
+ * pall - Print all the values on the stack
+ * @stack: Pointer to the stack
+ * @line_number: Line number of the instruction
+ */
+void pall(stack_t **stack, unsigned int line_number)
+{
+    (void)line_number;
+
+    stack_t *temp = *stack;
+    while (temp != NULL)
+    {
+        printf("%d\n", temp->n);
+        temp = temp->next;
+    }
+}
