a7e5c0bb5467db41d3b83fc7bcb5db462cc42c1e
Abdurehim <128035951+kedabdure@users.noreply.github.com>
2024-02-01 17:09:52 +0300

    Update monty.h

--- monty.h
+++ monty.h
@@ -4,6 +4,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
