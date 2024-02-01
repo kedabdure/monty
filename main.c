cde0aea7154b90fad778f53359416e215a458c3e
Abdurehim <128035951+kedabdure@users.noreply.github.com>
2024-02-02 02:49:03 +0300

    Update main.c
    the main file is updated

--- main.c
+++ main.c
@@ -1,21 +1,34 @@
 #include "monty.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
- * main - Entry point
- * @argc: number of arguments
- * @argv: argument
+ * main - Entry point for the Monty interpreter
+ * @argc: Number of arguments passed to the program
+ * @argv: Array of argument strings
  *
- * Return: 0
+ * Return: EXIT_SUCCESS on success, EXIT_FAILURE on failure
  */
 int main(int argc, char *argv[])
 {
-	if (argc != 2)
-	{
-		fprintf(stderr, "USAGE: monty file\n");
-		exit(EXIT_FAILURE);
-	}
+    FILE *file;
+    stack_t *stack = NULL;
 
-	readMontyFile(argv[1]);
-	return (0);
+    if (argc != 2)
+    {
+        fprintf(stderr, "Usage: %s file\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+    file = fopen(argv[1], "r");
+    if (file == NULL)
+    {
+        fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+        exit(EXIT_FAILURE);
+    }
+    parse_monty(file, &stack);
+    fclose(file);
+	
+    free_stack(stack);
+
+    return EXIT_SUCCESS;
 }
-
