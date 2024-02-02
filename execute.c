ee1338030ab8fcad5244450e5d06731e258de13e
Abdurehim <128035951+kedabdure@users.noreply.github.com>
2024-02-02 05:33:43 +0300

    Update execute.c

--- execute.c
+++ execute.c
@@ -3,11 +3,14 @@
 #include <stdio.h>
 #include <string.h>
 instruction_t instructions[] = {
-	{"push", push},
-	{"pall", pall},
-	{"pint", pint},
-	{"pop", pop},
-	{NULL, NULL}
+    {"push", push},
+    {"pall", pall},
+    {"pint", pint},
+    {"pop", pop},
+    {"swap", swap},
+    {"add", add},
+    {"nop", nop},
+    {NULL, NULL}
 };
 
 /**
