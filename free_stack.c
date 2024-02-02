360f9c02490248794de4d19692b05b6cda7fbfa2
Abdurehim <128035951+kedabdure@users.noreply.github.com>
2024-02-02 04:09:35 +0300

    Update free_stack.c

--- free_stack.c
+++ free_stack.c
@@ -1,18 +1,20 @@
 #include "monty.h"
 
 /**
- * free_stack - frees a doubly linked list
- * @stack: head of the stack
+ * free_stack - Frees a doubly linked list
+ * @stack: Head of the stack
+ *
+ * Description: Frees a doubly linked list.
  */
 void free_stack(stack_t *stack)
 {
-    stack_t *current = stack;
-    stack_t *next;
+	tabstack_t *current = stack;
+	tabstack_t *next;
 
-    while (current != NULL)
-    {
-        next = current->next;
-        free(current);
-        current = next;
-    }
+	while (current != NULL)
+	{
+		next = current->next;
+		free(current);
+		current = next;
+	}
 }
