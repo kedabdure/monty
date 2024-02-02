c549e055169b029702232156e1ece98eae42bf00
Abdurehim <128035951+kedabdure@users.noreply.github.com>
2024-02-02 05:38:19 +0300

    Update execute.c

--- execute.c
+++ execute.c
@@ -2,16 +2,6 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
-instruction_t instructions[] = {
-    {"push", push},
-    {"pall", pall},
-    {"pint", pint},
-    {"pop", pop},
-    {"swap", swap},
-    {"add", add},
-    {"nop", nop},
-    {NULL, NULL}
-};
 
 /**
  * execute - Execute Monty bytecode instructions
@@ -22,6 +12,16 @@ instruction_t instructions[] = {
 void execute(char *opcode, stack_t **stack, unsigned int line_number)
 {
 	int i = 0;
+	instruction_t instructions[] = {
+		{"push", push},
+		{"pall", pall},
+		{"pint", pint},
+		{"pop", pop},
+		{"swap", swap},
+		{"add", add},
+		{"nop", nop},
+		{NULL, NULL}
+	};
 	while (instructions[i].opcode != NULL)
 	{
 		if (strcmp(opcode, instructions[i].opcode) == 0)
