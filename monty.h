72d5ad5f48ebb1852af74d3a7d71de55e26c6996
Abdurehim <128035951+kedabdure@users.noreply.github.com>
2024-02-02 05:22:15 +0300

    Update monty.h

--- monty.h
+++ monty.h
@@ -45,5 +45,8 @@ void push(stack_t **stack, unsigned int line_number);
 void pop(stack_t **stack, unsigned int line_number);
 void pint(stack_t **stack, unsigned int line_number);
 void pall(stack_t **stack, unsigned int line_number);
+void swap(stack_t **stack, unsigned int line_number);
+void add(stack_t **stack, unsigned int line_number);
+void nop(stack_t **stack, unsigned int line_number);
 
 #endif /* MONTY_H */
