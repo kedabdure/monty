11885e5370ae95e39d3915dd557808ef6b5bbb1a
Abdurehim <128035951+kedabdure@users.noreply.github.com>
2024-02-02 04:11:36 +0300

    Update main.c

--- main.c
+++ main.c
@@ -11,24 +11,24 @@
  */
 int main(int argc, char *argv[])
 {
-    FILE *file;
-    stack_t *stack = NULL;
+	FILE *file;
+	stack_t *stack = NULL;
 
-    if (argc != 2)
-    {
-        fprintf(stderr, "USAGE: monty file\n");
-	return (EXIT_FAILURE);
-    }
-    file = fopen(argv[1], "r");
-    if (file == NULL)
-    {
-        fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
-	return (EXIT_FAILURE);
-    }
-    parse_monty(file, &stack);
-    fclose(file);
+	if (argc != 2)
+	{
+		fprintf(stderr, "USAGE: monty file\n");
+		return (EXIT_FAILURE);
+	}
+	file = fopen(argv[1], "r");
+	if (file == NULL)
+	{
+		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+		return (EXIT_FAILURE);
+	}
+	parse_monty(file, &stack);
+	fclose(file);
 	
-    free_stack(stack);
+	free_stack(stack);
 
-    return EXIT_SUCCESS;
+	return EXIT_SUCCESS;
 }
