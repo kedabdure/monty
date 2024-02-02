5443a2580d3a905f421d6305d190a637fa8fadcc
Abdurehim <128035951+kedabdure@users.noreply.github.com>
2024-02-02 05:11:47 +0300

    Update stack_operations2.c

--- stack_operations2.c
+++ stack_operations2.c
@@ -7,22 +7,22 @@
  */
 void swap(stack_t **stack, unsigned int line_number)
 {
-		stack_t *temp;
+	stack_t *temp;
 
-		if (*stack == NULL || (*stack)->next == NULL)
-		{
-				fprintf(stderr, "L%u: can't swap, stack too short\n", line_number);
-				exit(EXIT_FAILURE);
-		}
+	if (*stack == NULL || (*stack)->next == NULL)
+	{
+		fprintf(stderr, "L%u: can't swap, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
 
-		temp = (*stack)->next;
-		(*stack)->next = temp->next;
-		if (temp->next != NULL)
-				temp->next->prev = *stack;
-		temp->prev = NULL;
-		temp->next = *stack;
-		(*stack)->prev = temp;
-		*stack = temp;
+	temp = (*stack)->next;
+	(*stack)->next = temp->next;
+	if (temp->next != NULL)
+		temp->next->prev = *stack;
+	temp->prev = NULL;
+	temp->next = *stack;
+	(*stack)->prev = temp;
+	*stack = temp;
 }
 
 /**
@@ -32,19 +32,19 @@ void swap(stack_t **stack, unsigned int line_number)
  */
 void add(stack_t **stack, unsigned int line_number)
 {
-    stack_t *temp;
+	stack_t *temp;
 
-		if (*stack == NULL || (*stack)->next == NULL)
-		{
-				fprintf(stderr, "L%u: can't add, stack too short\n", line_number);
-				exit(EXIT_FAILURE);
-		}
+	if (*stack == NULL || (*stack)->next == NULL)
+	{
+		fprintf(stderr, "L%u: can't add, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
 
-		(*stack)->next->n += (*stack)->n;
-		temp = *stack;
-		*stack = (*stack)->next;
-		free(temp);
-		(*stack)->prev = NULL;
+	(*stack)->next->n += (*stack)->n;
+	temp = *stack;
+	*stack = (*stack)->next;
+	free(temp);
+	(*stack)->prev = NULL;
 }
 
 /**
@@ -54,6 +54,6 @@ void add(stack_t **stack, unsigned int line_number)
  */
 void nop(stack_t **stack, unsigned int line_number)
 {
-		(void)stack;
-		(void)line_number;
+	(void)stack;
+	(void)line_number;
 }
