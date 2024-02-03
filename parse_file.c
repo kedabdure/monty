8d356dab350e21a6e9892c90c748ca3ccaec3144
Abdurehim <128035951+kedabdure@users.noreply.github.com>
2024-02-03 14:47:46 +0300

    Update parse_file.c

--- parse_file.c
+++ parse_file.c
@@ -7,32 +7,66 @@
  */
 void parse_monty(FILE *file, stack_t **stack)
 {
-	char *line = NULL;
-	size_t len = 0;
-	unsigned int line_number = 1;
+    char *line = NULL;
+    size_t len = 0;
+    unsigned int line_number = 1;
 
-	char *arg, *opcode;
+    while (getline(&line, &len, file) != -1)
+    {
+        tokenize_line(line);
 
-	while (fgets(line, len, file) != NULL)
-	{
-		opcode = strtok(line, " \t\n");
+        if (op_toks_count > 0)
+        {
+            execute(op_toks[0], stack, line_number);
 
-		if (opcode == NULL || opcode[0] == '#')
-		{
-			line_number++;
-			continue;
-		}
-		arg = strtok(NULL, " \t\n");
+            // Free memory allocated for tokens
+            for (int i = 0; i < op_toks_count; i++)
+            {
+                free(op_toks[i]);
+            }
+            free(op_toks);
+        }
 
-		if (arg != NULL)
-		{
-			value = atoi(arg);
-		}
+        line_number++;
+    }
 
-		execute(opcode, stack, line_number);
+    free(line);
+}
 
-		line_number++;
-	}
+/**
+ * tokenize_line - Tokenize a line and store tokens in op_toks
+ * @line: Line to tokenize
+ */
+void tokenize_line(char *line)
+{
+    char *token;
+    int token_count = 0;
 
-	free(line);
+    // Allocate memory for op_toks
+    op_toks = (char **)malloc(MAX_TOKENS * sizeof(char *));
+    if (op_toks == NULL)
+    {
+        fprintf(stderr, "Error: Unable to allocate memory for op_toks\n");
+        exit(EXIT_FAILURE);
+    }
+
+    // Tokenize the line
+    token = strtok(line, " \t\n");
+    while (token != NULL && token_count < MAX_TOKENS)
+    {
+        // Allocate memory for each token
+        op_toks[token_count] = strdup(token);
+        if (op_toks[token_count] == NULL)
+        {
+            fprintf(stderr, "Error: Unable to allocate memory for token\n");
+            exit(EXIT_FAILURE);
+        }
+
+        // Update token count and get the next token
+        token_count++;
+        token = strtok(NULL, " \t\n");
+    }
+
+    // Update the count of tokenized lines
+    op_toks_count = token_count;
 }
