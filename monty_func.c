b39b59317ad456be4443187dcd3b40f83d41e1a2
fuadhasen <fuadhas6634@gmail.com>
2024-02-03 10:55:21 -0800

    pint

--- monty_func.c
+++ monty_func.c
@@ -11,7 +11,7 @@ void pint(stack_t **head, unsigned int line_num)
 {
 	if (*head == NULL)
 	{
-		fprintf(stderr, "L%u: can't pint, stack empty", line_num);
+		fprintf(stderr, "L%u: can't pint, stack empty\n", line_num);
 		exit(EXIT_FAILURE);
 	}
 
