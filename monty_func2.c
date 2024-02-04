1b508705f5fcbf0f70db15532e0407d5bedb909e
Abdurehim <128035951+kedabdure@users.noreply.github.com>
2024-02-04 15:57:01 +0300

    Update monty_func2.c

--- monty_func2.c
+++ monty_func2.c
@@ -39,3 +39,36 @@ void sub(stack_t **head, unsigned int line_number)
 		(*head)->prev = NULL;
 	(*head)->n = result;
 }
+/**
+ * sub - Subtracts the top element of the stack from the second top element.
+ * @head: Pointer to the head of the stack (doubly linked list).
+ * @line_number: Line number of the instruction.
+ */
+void sub(stack_t **head, unsigned int line_number)
+{
+	int num1, num2, result;
+	stack_t *temp;
+
+	if (*head == NULL || (*head)->next == NULL)
+	{
+			fprintf(stderr, "L%u: can't sub, stack too short\n", line_number);
+			exit(EXIT_FAILURE);
+	}
+
+	num1 = (*head)->n;
+	num2 = (*head)->next->n;
+
+	result = num2 / num1;
+	if(num1 == 0)
+	{
+		fprintf(stderr, "L%u: division by zero\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	temp = *head;
+	*head = (*head)->next;
+	free(temp);
+	if (*head != NULL)
+		(*head)->prev = NULL;
+	(*head)->n = result;
+}
+
