6357b3e28780655cc3ba813d4d5d8c34776cc66b
Abdurehim <128035951+kedabdure@users.noreply.github.com>
2024-02-04 16:00:58 +0300

    Update monty.h

--- monty.h
+++ monty.h
@@ -51,6 +51,7 @@ void pop(stack_t **head, unsigned int line_num);
 void add(stack_t **head, unsigned int line_num);
 void nop(stack_t **head, unsigned int line_num);
 void sub(stack_t **head, unsigned int line_number);
+void _div(stack_t **head, unsigned int line_number);
 void mul(stack_t **head, unsigned int line_number);
 void mod(stack_t **head, unsigned int line_number);
 #endif
