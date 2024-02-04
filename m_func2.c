bd79063d1000acc0be508e3c8c78882b12a21c74
fuadhasen <fuadhas6634@gmail.com>
2024-02-04 03:51:46 -0800

    add

--- m_func2.c
+++ m_func2.c
@@ -10,16 +10,16 @@
 
 void add(stack_t **head, unsigned int line_num)
 {
-	stack_t *temp1 = *head;
-	stack_t *temp = (*head)->next;
-
+	stack_t *temp1, *temp2;
 	if (*head == NULL || (*head)->next == NULL)
 	{
-		fprintf(stderr, "L%u: can't add, stack too short", line_num);
+		fprintf(stderr, "L%u: can't add, stack too short\n", line_num);
 		exit(EXIT_FAILURE);
 	}
 
-	temp->n = temp->n + (*head)->n;
+	temp1 = *head;
+	temp2 = (*head)->next;
+	temp2->n = temp2->n + (*head)->n;
 	if ((*head)->next != NULL)
 		*head = (*head)->next;
 	free(temp1);
